411dd9f7b21f8dcb7cf270818479e72ca507738a
DucPT <ducft.nb@gmail.com>
2017-04-19 06:45:50 +0700

    Create base CoreData

---
+++ CoreData_Big0/CoreData_Big0/CoreDataTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataTableCell.swift
+//  CoreData_Big0
+//
+//  Created by DucPT on 4/19/17.
+//  Copyright © 2017 DucPT. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CoreData_Big0/CoreData_Big0/CoreDataTableVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData_Big0
+//
+//  Created by DucPT on 4/19/17.
+//  Copyright © 2017 DucPT. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
